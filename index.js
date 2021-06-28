a8128c0e3824d21b9b5a80618de8b3ae526c9ad8
yurukyuruk <yurukyuruk@gmail.com>
2021-06-28 21:14:20 +0300

    updated game

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 function userGuess(number) {  
@@ -24,6 +25,61 @@ while(true) {
     break;
   } 
 }
+*/
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let typingArea = document.querySelector(".typing-area");
+const guessHint = document.querySelector(".guess-hint");
+const questionMark = document.querySelector(".question-mark");
+
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  let comment;
+  const correctAnswer = "CORRECT ANSWER";
+  const veryCloseAnswer = "VERY CLOSE";
+  const closeAnswer = "CLOSE";
+  const farAnswer = "FAR";
+  
+  if(randomNumber == typingArea.value) {
+    comment = correctAnswer;
+    questionMark.innerHTML = randomNumber;
+  } else if(typingArea.value < 10) {
+      if(Math.abs(randomNumber - typingArea.value) === 1) {
+        comment = veryCloseAnswer;    
+      } else if(Math.abs(randomNumber - typingArea.value) < 6) {
+        comment = closeAnswer;
+      } else {
+        comment = farAnswer;
+      }
+  } else {
+      if(Math.abs(randomNumber - typingArea.value) < 11) {
+        comment = veryCloseAnswer; 
+      } else if(Math.abs(randomNumber - typingArea.value) < 51) {
+        comment = closeAnswer;
+      } else {
+        comment = farAnswer;
+      }
+  };
+
+  let historyList = document.createElement("li");
+  historyList.className = "list-elements";
+  ul.append(historyList);
+  historyList.innerHTML = typingArea.value + " " + comment;
+  guessHint.innerHTML = comment;
+  typingArea.value = ""; 
+});
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", function(event) {
+  guessHint.innerHTML = "";
+  questionMark.innerHTML = "?";
+  const guessList = document.querySelectorAll(".list-elements");
+  for(let i = 0; i < guessList.length; i++) {
+    guessList[i].remove();
+  }
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+});
 
 
 
